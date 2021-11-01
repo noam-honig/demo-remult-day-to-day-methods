d30d142b4fc582ba8afeeae5224d1ac372b2ca00
Noam <noam@fireflymigration.com>
2021-11-01 10:33:05 +0200

    using lambda for a specfic entity

--- src/app/demo-validators/demo-validators-entity.ts
+++ src/app/demo-validators/demo-validators-entity.ts
@@ -2,8 +2,11 @@ import { Entity, Field, IdEntity, Validators } from "remult";
 
 @Entity("demovalidatorsentity", { allowApiCrud: true })
 export class DemoValidatorsEntity extends IdEntity {
-    @Field({
-        validate:Validators.required.withMessage("bla bla")
+    @Field<DemoValidatorsEntity, string>({
+        validate: (self, col) => {
+            if (self.a=='')
+                self.$.a.error = "bbbb";
+        }
     })
     a: string = '';
 }
