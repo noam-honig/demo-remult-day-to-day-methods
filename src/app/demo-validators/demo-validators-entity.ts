24129728bc69b3807605814f8d39d233288046a5
Noam <noam@fireflymigration.com>
2021-11-01 10:30:42 +0200

    with message

--- src/app/demo-validators/demo-validators-entity.ts
+++ src/app/demo-validators/demo-validators-entity.ts
@@ -3,7 +3,7 @@ import { Entity, Field, IdEntity, Validators } from "remult";
 @Entity("demovalidatorsentity", { allowApiCrud: true })
 export class DemoValidatorsEntity extends IdEntity {
     @Field({
-        validate:Validators.required
+        validate:Validators.required.withMessage("bla bla")
     })
     a: string = '';
 }
